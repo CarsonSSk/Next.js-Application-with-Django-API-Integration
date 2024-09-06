11ac9266d3af47c350bcbb0b013cd00b42905cb8
CarsonSSk <carson_skumar@hotmail.com>
2024-09-06 01:04:42 -0400

    Revert "adding backend code"
    This reverts commit e84e79bb866e2fb24c0e2f900eb52178a7b605bd.

--- my_backend/my_backend/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('charts.urls')),  # This includes the URLs from the charts app
-]
